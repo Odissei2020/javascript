2a78d6e0fc5b17749c90a45304dd422647ec2fd4
Ruslan <lider-1@mail.ru>
2021-02-01 12:03:56 +0600

    Massivy

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let num = 50;
+
+/* while (num<=55) {
+    console.log(num);
+    num++;
+} */
+
+/* do {
+    num++;
+    console.log(num);
+}
+while (num < 55); */
+for  (let i = 1; i < 8; i++){
+    console.log(num);
+    num++;
+}
