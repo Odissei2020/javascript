964b53a5c4c97bd4f3cd0fb8f5795abf19c685c8
Ruslan <lider-1@mail.ru>
2021-02-01 12:32:14 +0600

    Vypolnenie Zadaniya Massivy

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+const numberDfFilms = +prompt ('Сколько фильмов вы уже смотрели?', '');
+
+const personalMovieDB = {
+    count: numberDfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+console.log(personalMovieDB);
