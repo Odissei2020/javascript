8ab458dc6b989fa210df655c5cade30e19c26619
Ruslan <lider-1@mail.ru>
2021-02-02 09:44:23 +0600

    function

--- js/script.js
+++ js/script.js
@@ -14,42 +14,8 @@
 
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
-
-const numberDfFilms = +prompt ('Сколько фильмов вы уже смотрели?', '');
-
-const personalMovieDB = {
-    count: numberDfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-let i = 0;
-
-do {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-          i++;
-
-    if (a != null && b != null && a != '' && b !='' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done'); 
-        } else {
-        console.log('error');
-        i--;
-        }
-} while ( i < 2);
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) { 
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function showFirstMessage() {
+    console.log('Hello World');
 }
 
-console.log(personalMovieDB);
+showFirstMessage();
