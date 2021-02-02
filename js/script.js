1fe0c83235aa3adee87706fccc9ade3dea98a849
Ruslan <lider-1@mail.ru>
2021-02-02 10:12:11 +0600

    funstion peremenye2

--- js/script.js
+++ js/script.js
@@ -18,9 +18,10 @@ let num = 20;
 
 function showFirstMessage(text) {
     console.log(text);
-    num = 100;
+    let num = 10;
     console.log(num);
 }
 
 showFirstMessage('Hello World');
 console.log(num);
+
