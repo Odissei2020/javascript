569fa07fc92ad1b08b62d9898ccc6fb703931fad
Ruslan <lider-1@mail.ru>
2021-02-02 10:41:08 +0600

    RETURN

--- js/script.js
+++ js/script.js
@@ -25,3 +25,19 @@ function showFirstMessage(text) {
 showFirstMessage('Hello World');
 console.log(num);
 
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    //
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
