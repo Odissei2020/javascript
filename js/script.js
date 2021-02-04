6a1f58d6f66497b2b1e08dc5843c87c735413a20
Ruslan <lider-1@mail.ru>
2021-02-04 11:49:18 +0600

    Zadanie 3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,85 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-const str = "test";
-/* const arr = [1, 4, 6];
+// Код возьмите из предыдущего домашнего задания
 
-console.log(str.toUpperCase()); */
-console.log(str.toLowerCase()); 
-console.log(str);
+let numberDfFilms;
+
+function start() {
+  numberDfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberDfFilms == '' || numberDfFilms == null || isNaN(numberDfFilms)) {
+    numberDfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+ start();
+
+const personalMovieDB = {
+    count: numberDfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && a.length<50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }    
+
+}
+
+/* rememberMyFilms(); */
+
+function detectPersonaLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы класический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log("Произошла ошибка");
+    }    
+}
+
+/* detectPersonaLevel(); */
+
+function showMyDB (hidden){
+    if (!hidden) {
+        console.log(personalMovieDB);
+}  
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3 ; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
