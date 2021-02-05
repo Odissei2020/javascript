da4d3e18e08653aaff53cb2d0761a4b78585a5a5
Ruslan <lider-1@mail.ru>
2021-02-05 13:07:00 +0600

    Obecty

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 
 'use strict';
 
-// Функция Callback
+// Объекты ассоциативные массивы
 
-function first() {
-    //Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+/* let counter = 0; */
+
+const options = {
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    color: {
+        border: `black`,
+        bg: `red`
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+console.log(Object.keys(options).length);
+
+
+/* console.log(options.color.bg);
+console.log(options.color[`border`]);
+delete options.width;
+console.log(options); */
+
+/* for (let key in options) {
+    if (typeof(options[key]) === `object`) {
+        for (let i in options [key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойства ${key} имеет значение ${options[key]}`);
+        counter++;
+    }   
 }
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done () {
-    console.log('Я прошел этот урок');
-}
-
-learnJS('JavaScript', done);
+console.log(counter); */
